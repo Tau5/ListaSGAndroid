be43d9a17be5572c2922c3a07ecd17e5bcbf3a23
chuwi <srpc9682@protonmail.com>
2025-12-16 09:51:53 +0100

    Rename class

--- app/src/main/java/com/example/listasg/Series.kt
+++ app/src/main/java/com/example/listasg/Series.kt
@@ -2,7 +2,7 @@ package com.example.listasg
 
 import androidx.annotation.DrawableRes
 
-data class Item(
+data class Series(
     val title: String,
     @DrawableRes val image: Int,
     val description: String
